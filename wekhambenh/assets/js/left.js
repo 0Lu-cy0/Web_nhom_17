573d8df603201e6c23e97a76593f1f34c33605a0
0Lu-cy0 <nc12042004@gmail.com>
2024-05-21 22:26:04 +0700

    fix các trang nhấp nháy

--- wekhambenh/assets/js/left.js
+++ wekhambenh/assets/js/left.js
@@ -1,25 +1,4 @@
-// $(document).ready(function() {
-//   // Xử lý khi nhấp vào mục "Home"
-//   $("#homeButton").click(function() {
-//       window.location.href = "trang_tong_quan.html"; // Thay thế 'home.html' bằng đường dẫn thực tế của trang Home
-//   });
-
-//   // Xử lý khi nhấp vào mục "Appointments"
-//   $("#appointmentsButton").click(function() {
-//       window.location.href = "cuoc_hen.html"; // Thay thế 'appointments.html' bằng đường dẫn thực tế của trang Appointments
-//   });
-
-//   // Xử lý khi nhấp vào mục "Doctors"
-//   $("#doctorsButton").click(function() {
-//       window.location.href = "bac_si.html"; // Thay thế 'doctors.html' bằng đường dẫn thực tế của trang Doctors
-//   });
-
-//   // Xử lý khi nhấp vào mục "About"
-//   $("#aboutButton").click(function() {
-//       window.location.href = "trang_tong_quan_about.html"; // Thay thế 'about.html' bằng đường dẫn thực tế của trang About
-//   });
-// });
-
 $(function(){
     $("#left").load("left.html"); 
   });
+  

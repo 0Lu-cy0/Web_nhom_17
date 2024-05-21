8e4d53edabac7c3732f6812b772f0b418b7bb7a9
frozenkiss04 <dinhphan1804@gmail.com>
2024-05-21 18:30:41 +0700

    trang tong quan about js

--- wekhambenh/assets/js/header.js
+++ wekhambenh/assets/js/header.js
@@ -1,15 +1,19 @@
 
-  // Xử lý khi nhấn vào nút Đăng xuất
-  document.getElementById('logoutButton').addEventListener('click', function() {
-      // Xóa thông tin người dùng (nếu có)
-      localStorage.removeItem('firstName');
-      localStorage.removeItem('lastName');
-      localStorage.removeItem('address');
-      localStorage.removeItem('phoneNumber');
-      localStorage.removeItem('emailAddress');
-      localStorage.removeItem('password');
-      localStorage.removeItem('profileImage');
+  // // Xử lý khi nhấn vào nút Đăng xuất
+  // document.getElementById('logoutButton').addEventListener('click', function() {
+  //     // Xóa thông tin người dùng (nếu có)
+  //     localStorage.removeItem('firstName');
+  //     localStorage.removeItem('lastName');
+  //     localStorage.removeItem('address');
+  //     localStorage.removeItem('phoneNumber');
+  //     localStorage.removeItem('emailAddress');
+  //     localStorage.removeItem('password');
+  //     localStorage.removeItem('profileImage');
 
-      // Chuyển hướng đến trang Đăng nhập
-      window.location.href = 'dang_nhap.html'; // Thay thế 'login.html' bằng đường dẫn thực tế của trang Đăng nhập
-  }); 
+  //     // Chuyển hướng đến trang Đăng nhập
+  //     window.location.href = 'dang_nhap.html'; // Thay thế 'login.html' bằng đường dẫn thực tế của trang Đăng nhập
+  // }); 
+
+  $(function(){
+    $("#header").load("header.html"); 
+  });
--- wekhambenh/assets/js/left.js
+++ wekhambenh/assets/js/left.js
@@ -1,24 +1,24 @@
-$(document).ready(function() {
-  // Xử lý khi nhấp vào mục "Home"
-  $("#homeButton").click(function() {
-      window.location.href = "trang_tong_quan.html"; // Thay thế 'home.html' bằng đường dẫn thực tế của trang Home
-  });
+// $(document).ready(function() {
+//   // Xử lý khi nhấp vào mục "Home"
+//   $("#homeButton").click(function() {
+//       window.location.href = "trang_tong_quan.html"; // Thay thế 'home.html' bằng đường dẫn thực tế của trang Home
+//   });
 
-  // Xử lý khi nhấp vào mục "Appointments"
-  $("#appointmentsButton").click(function() {
-      window.location.href = "cuoc_hen.html"; // Thay thế 'appointments.html' bằng đường dẫn thực tế của trang Appointments
-  });
+//   // Xử lý khi nhấp vào mục "Appointments"
+//   $("#appointmentsButton").click(function() {
+//       window.location.href = "cuoc_hen.html"; // Thay thế 'appointments.html' bằng đường dẫn thực tế của trang Appointments
+//   });
 
-  // Xử lý khi nhấp vào mục "Doctors"
-  $("#doctorsButton").click(function() {
-      window.location.href = "bac_si.html"; // Thay thế 'doctors.html' bằng đường dẫn thực tế của trang Doctors
-  });
+//   // Xử lý khi nhấp vào mục "Doctors"
+//   $("#doctorsButton").click(function() {
+//       window.location.href = "bac_si.html"; // Thay thế 'doctors.html' bằng đường dẫn thực tế của trang Doctors
+//   });
 
-  // Xử lý khi nhấp vào mục "About"
-  $("#aboutButton").click(function() {
-      window.location.href = "trang_tong_quan_about.html"; // Thay thế 'about.html' bằng đường dẫn thực tế của trang About
-  });
-});
+//   // Xử lý khi nhấp vào mục "About"
+//   $("#aboutButton").click(function() {
+//       window.location.href = "trang_tong_quan_about.html"; // Thay thế 'about.html' bằng đường dẫn thực tế của trang About
+//   });
+// });
 
 $(function(){
     $("#left").load("left.html"); 

943a423c13b5e13d6e340b179d42c59a35f36c33
0Lu-cy0 <nc12042004@gmail.com>
2024-05-23 21:00:21 +0700

    Thêm js cho đăng nhập, đăng xuất, thêm js phần left

--- wekhambenh/assets/js/header.js
+++ wekhambenh/assets/js/header.js
@@ -1,4 +1,9 @@
 $(function(){
-    $("#header").load("header.html"); 
-  });
-  
+  $("#header").load("header.html"); 
+});
+
+function logout() {
+  alert('You have been logged out.');
+  localStorage.removeItem('selectedPage'); // Xóa trạng thái đã chọn khi đăng xuất
+  window.location.href = 'dang_nhap.html'; // Chuyển hướng đến trang đăng nhập
+}
--- wekhambenh/assets/js/left.js
+++ wekhambenh/assets/js/left.js
@@ -1,4 +1,20 @@
-$(function(){
-    $("#left").load("left.html"); 
+function navigateToPage(page) {
+  localStorage.setItem('selectedPage', page);
+  window.location.href = page;
+}
+
+function loadMenu() {
+  var selectedPage = localStorage.getItem('selectedPage');
+  if (selectedPage) {
+    var selectedFrame = document.querySelector('[onclick="navigateToPage(\'' + selectedPage + '\')"]');
+    if (selectedFrame) {
+      selectedFrame.classList.add('selected');
+    }
+  }
+}
+
+$(function () {
+  $("#left").load("left.html", function () {
+    loadMenu();
   });
-  
+});
